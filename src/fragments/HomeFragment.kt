c925e783d63e4ce824d0968fdd12f9b6db5531b9
Sakshi09-eng <ssingh28011996@gmail.com>
2025-04-19 12:27:39 +0530

    Add src/fragments/HomeFragment.kt 1745045859017

---
+++ src/fragments/HomeFragment.kt
@@ -0,0 +1,27 @@
+package com.example.myapp.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import android.widget.TextView
+import android.widget.FrameLayout
+import android.view.Gravity
+import android.graphics.Color
+
+class HomeFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val textView = TextView(activity)
+        textView.text = "Home"
+        textView.textSize = 24f
+        textView.setTextColor(Color.BLACK)
+        textView.gravity = Gravity.CENTER
+        val layout = FrameLayout(activity!!)
+        layout.addView(textView)
+        return layout
+    }
+}
